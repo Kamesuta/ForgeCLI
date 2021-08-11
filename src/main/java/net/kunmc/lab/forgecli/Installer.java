5a1b0a00bbddd6abaa79ba35338e2e631d549aa6
Kamesuta <kamesuta@gmail.com>
2021-08-12 03:34:34 +0900

    Setup

---
+++ src/main/java/net/kunmc/lab/forgecli/Installer.java
@@ -0,0 +1,23 @@
+package net.kunmc.lab.forgecli;
+
+import java.io.File;
+
+import net.kunmc.lab.forgecli.impl.InstallerUtil;
+import net.minecraftforge.installer.actions.ProgressCallback;
+import net.minecraftforge.installer.json.Install;
+
+public class Installer {
+    public static boolean install(File target, File installerJar, String forgeVersion) {
+        ProgressCallback monitor = ProgressCallback.withOutputs(System.out);
+        Install install = InstallerUtil.loadInstallProfile(forgeVersion);
+        if (System.getProperty("java.net.preferIPv4Stack") == null) {
+            System.setProperty("java.net.preferIPv4Stack", "true");
+        }
+        String vendor = System.getProperty("java.vendor", "missing vendor");
+        String javaVersion = System.getProperty("java.version", "missing java version");
+        String jvmVersion = System.getProperty("java.vm.version", "missing jvm version");
+        monitor.message(String.format("JVM info: %s - %s - %s", vendor, javaVersion, jvmVersion));
+        monitor.message("java.net.preferIPv4Stack=" + System.getProperty("java.net.preferIPv4Stack"));
+        return InstallerUtil.runClientInstall(forgeVersion, install, monitor, target, installerJar);
+    }
+}
---
+++ src/main/java/net/kunmc/lab/forgecli/impl/ClientInstall.java
@@ -0,0 +1,20 @@
+package net.kunmc.lab.forgecli.impl;
+
+import net.minecraftforge.installer.actions.ActionCanceledException;
+import net.minecraftforge.installer.actions.ProgressCallback;
+import net.minecraftforge.installer.json.Install;
+import net.minecraftforge.installer.json.InstallV1;
+
+import java.io.File;
+import java.util.function.Predicate;
+
+public class ClientInstall extends net.minecraftforge.installer.actions.ClientInstall {
+    public ClientInstall(Install profile, ProgressCallback monitor) {
+        super(profile instanceof InstallV1 ? (InstallV1) profile : new InstallV1(profile), monitor);
+    }
+
+    @Override
+    public boolean run(File target, Predicate<String> optionals, File installer) throws ActionCanceledException {
+        return super.run(target, optionals, installer);
+    }
+}
